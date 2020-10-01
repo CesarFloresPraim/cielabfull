186d2eaaab41cb46aa9884b251d659818e886df7
Cesar <cesar_florespal@outlook.com>
2020-09-30 18:15:46 -0600

    Changes proposed on september 29

--- front-proy/public/assets/js/myMain.js
+++ front-proy/public/assets/js/myMain.js
@@ -6,21 +6,29 @@
     $("#programa_link_conservacion").click(function() {
         $("#programas_contenido").html(
             `<li class="nav-item"><div>
-            <p>El programa de conservación va ligado tanto al programa de educación como al de investigación, ya que desde el ámbito educativo se desarrollan talleres de conservación del patrimonio que permiten dar a conocer las técnicas y criterios de conservación adaptados a la arquitectura vernácula; además, bajo criterios científicos de investigación y toma de datos en campo se definen las bases para identificar los materiales de manufactura así como las causas y grado de deterioro de los bienes arquitectónicos y artísticos.
+
+            <p>
+            El programa de conservación va ligado tanto al programa de educación como al de investigación, ya que desde el ámbito educativo se desarrollan talleres de conservación del patrimonio que permiten dar a conocer las técnicas y criterios de conservación adaptados a la arquitectura vernácula; además, bajo criterios científicos de investigación y toma de datos en campo, se definen las bases para identificar los materiales de manufactura así como las causas y grado de deterioro de los bienes arquitectónicos y artísticos.
             </p>
             <br>
-            <p>El programa de conservación de CIE@Lab tiene el objetivo de establecer un repositorio en el que se alberguen las experiencias de conservación del patrimonio, incluyendo aquellas acciones experimentales y sus resultados, este repositorio tendrá una cualidad colaborativa que permitirá a la propia comunidad y personas interesadas consultarlo y aportar a su acervo. La organización y compilación de todos los materiales asociados al patrimonio cultural de Valle de Allende se convertirá en una herramienta proveedora de nuevos datos e información para el ejercicio de una conservación integrada a la investigación y educación de este vasto legado patrimonial.
+            <p>
+            El programa de conservación de CIE@Lab tiene el objetivo de establecer un repositorio en el que se alberguen las experiencias de conservación del patrimonio, incluyendo aquellas acciones experimentales y sus resultados. Este repositorio tiene una cualidad colaborativa que permite a la propia comunidad y personas interesadas a consultarlo, así como aportar a su acervo. La organización y compilación de materiales asociados al patrimonio cultural de los sitios seleccionados como centros de investigación -laboratorios-, se convertirá en una herramienta proveedora de nuevos datos e información para el ejercicio de una conservación integrada a la investigación y educación del vasto legado patrimonial del estado de Chihuahua.
             </p>
             <br>
             <ul>
               <h4>Actividades principales:</h4>
-              <li>Establecer protocolos para el análisis visual para determinar el estado de conservación de de bienes inmuebles, inmuebles por destino y muebles de carácter e interés histórico-cultural.
+              <li>
+Establecer protocolos para el análisis visual para determinar el estado de conservación de de bienes inmuebles, inmuebles por destino y muebles de carácter e interés histórico-cultural.
               </li>
-              <li>Establecer protocolos para la intervención de bienes arquitectónicos y artísticos de históricos y culturales del estado de Chihuahua.
+              <li>
+Establecer protocolos para la intervención de bienes arquitectónicos y artísticos de históricos y culturales del estado de Chihuahua.
               </li>
-              <li>Establecer un repositorio documental que albergue investigaciones documentales, y técnico-científicas de materiales histórico y patrimoniales
+              <li>
+Establecer un repositorio documental que albergue investigaciones documentales, y técnico-científicas de materiales histórico y patrimonial
               </li>
-              <li>Generar reporte técnico de los resultados.</li>
+              <li>
+              Generar reporte técnico de los resultados.
+              </li>
             </ul>
           </div></li>`
         )
@@ -28,29 +36,35 @@
       $("#programa_link_educacion").click(function() {
         $("#programas_contenido").html(
             `<li class="nav-item"><div>
-            <p>Es el programa donde se considera que los procesos educativos, vinculados al tema del patrimonio cultural, conocidos como educación patrimonial, no sólo son formales, sino que también  comprenden las prácticas educativas que se realizan en los ambientes no formales  e  informales. Esta educación patrimonial, considera también la interculturalidad como problema, se define por la carga de diversidad que contiene. Diversidad, que se muestra en la cultura y la educación, por lo que se toman en cuenta el lenguaje, los saberes y conocimientos que se construyen en su ethos, el “alma” de los pueblos.  Así mismo, podemos comprender que la educación, vista desde este sentido -más allá de la formalidad disciplinaria institucional- es el conducto, junto con otros planos de la realidad,  de la forma en que se gestan distintas formas psicopedagógicas específicas para que el ethos perdure y consolide la historia y geografía en que se construye. Por ello, en la interculturalidad no sólo se vislumbran relaciones y conflictos éticos, estéticos y étnicos de clases sociales, grupos e intereses de identidad, sino que también se asoman intereses políticos, de imaginarios y representaciones sociales. Así dentro de los objetivos generales de este programa está el de: 
+            <p>
+            Es el programa donde se considera que los procesos educativos, vinculados al tema del patrimonio cultural, conocidos como educación patrimonial, no sólo son formales, sino que también  comprenden las prácticas educativas que se realizan en los ambientes no formales . Esta educación patrimonial, considera también la interculturalidad como problema, se define por la carga de diversidad que contiene. Diversidad, que se muestra en la cultura y la educación, por lo que se toman en cuenta el lenguaje, los saberes y conocimientos que se construyen en su ethos, el “alma” de los pueblos.  Así mismo, podemos comprender que la educación, vista desde este sentido -más allá de la formalidad disciplinaria institucional- es el conducto, junto con otros planos de la realidad,  de la forma en que se gestan distintas formas psicopedagógicas específicas para que el ethos perdure y consolide la historia y geografía en que se construye. Por ello, en la interculturalidad no sólo se vislumbran relaciones y conflictos éticos, estéticos y étnicos de clases sociales, grupos e intereses de identidad, sino que también se asoman intereses políticos, de imaginarios y representaciones sociales. Así dentro de los objetivos generales de este programa está el de:
             </p>
             <br>
             <ul>
-              <li>Desarrollar y gestionar la interculturalidad en contextos educativos formales y no formales donde el patrimonio cultural e histórico, así como los procesos psicopedagógicos culturales constituyen elementos de fortalecimiento, promoción, divulgación y estudio de la educación patrimonial.  
+              <li>
+              Desarrollar y gestionar la interculturalidad en contextos educativos formales y no formales donde el patrimonio cultural e histórico, así como los procesos psicopedagógicos culturales constituyen elementos de fortalecimiento, promoción, divulgación y estudio de la educación patrimonial.
               </li>
-              <li>Realizar investigación básica y aplicada en estudios culturales para sustentar en corpus y en praxis la intervención educativa, en contextos formales, no formales e informales, para la conservación del patrimonio cultural.
+              <li>
+              Realizar investigación básica y aplicada en estudios culturales para sustentar en corpus y en praxis la intervención educativa, en contextos formales, no formales e informales, para la conservación del patrimonio cultural.
               </li>
-              <li>Identificar, diseñar y aplicar diversas estrategias pedagógicas socioculturales para la conservación y protección de los diversos patrimonios histórico-culturales gestados en diversos contextos. 
+              <li>
+              Identificar, diseñar y aplicar diversas estrategias pedagógicas socioculturales para la conservación y protección de los diversos patrimonios histórico-culturales gestados en diversos contextos.
               </li>
             </ul>
             <br>
             <ul>
               <h4>Actividades principales:</h4>
-              <li>Diseño y elaboración de protocolos educativos para la conservación del Patrimonio cultural material, inmaterial y biocultural de Valle de Allende. 
+              <li>
+Diseño y elaboración de protocolos educativos para la conservación del Patrimonio cultural material, inmaterial y biocultural de los centros de investigación establecidos denominados laboratorios.
               </li>
-              <li>Elaboración de materiales educativos con nuevas Tecnologías de la Información y de la Comunicación (TIC) para educación básica y media superior.
+              <li>
+Elaboración de materiales educativos con nuevas Tecnologías de la Información y de la Comunicación (TIC) para educación básica y media superior.
               </li>
-              <li>Desarrollo de estrategias  pedagógicas socioculturales (talleres participativos) para conservación del patrimonio cultural material, inmaterial y biocultural de Valle de Allende. 
+              <li>
+Desarrollo de estrategias  pedagógicas socioculturales (talleres participativos) para conservación del patrimonio cultural material, inmaterial y biocultural de los laboratorios.Diseño curricular para la elaboración de una especialidad en Gestión, Conservación y Educación del Patrimonio Cultural.
               </li>
-              <li>Diseño curricular para la elaboración de una especialidad en Gestión, Conservación y Educación del Patrimonio Cultural.
-              </li>
-              <li>Certificación de Maestros Albañiles en Conservación y Protección del Patrimonio Cultural.
+              <li>
+Certificación de Maestros Albañiles en Conservación y Protección del Patrimonio Cultural.
               </li>
             </ul>
           </div></li>`
@@ -59,28 +73,37 @@
       $("#programa_link_investigacion").click(function() {
         $("#programas_contenido").html(
             `<li class="nav-item"><div>
-            <p>El estudio científico de los materiales y técnicas de manufactura del Patrimonio Cultural tangible, aunada a la investigación documental, aporta nueva información histórica, social y económica de los pueblos originarios. El 96% del estado de Chihuahua cuenta con bienes patrimoniales de carácter etnográfico e histórico, por lo que es eminente establecer parámetros que permitan la correcta interpretación de su materialidad y técnica, para establecer protocolos y procesos de conservación que garanticen su correcta intervención. 
+            <p>
+            El estudio científico de los materiales y técnicas de manufactura del Patrimonio Cultural tangible, aunada a la investigación documental, aporta nueva información histórica, social y económica de los pueblos originarios. El 96% del estado de Chihuahua cuenta con bienes patrimoniales de carácter etnográfico e histórico, por lo que es eminente establecer parámetros que permitan la correcta interpretación de su materialidad y técnica, para establecer protocolos y procesos de conservación que garanticen su correcta intervención.
             </p>
             <br>
-            <p>Los materiales y las técnicas utilizadas en la construcción de arquitecturas patrimoniales interaccionan constantemente con factores físicos, químicos y biológicos, provenientes del medio ambiente que pueden ser agentes promotores del proceso de deterioro de bienes inmuebles. Por esa razón, el monitoreo de indicadores ambientales y la detección de agentes microbiológicos es crucial para el seguimiento y evaluación  del estado de conservación de los materiales que componen a los diferentes vestigios Patrimoniales, sobre todo en el Estado de Chihuahua, el cual se caracteriza por una amplia diversidad climática. 
+            <p>
+Los materiales y las técnicas utilizadas en la construcción de arquitecturas patrimoniales interaccionan constantemente con factores físicos, químicos y biológicos, provenientes de medio ambiente que pueden ser agentes promotores del proceso de deterioro de bienes inmuebles. Por esa razón, el monitoreo de indicadores ambientales y la detección de agentes microbiológicos es crucial para el seguimiento y evaluación  del estado de conservación de los materiales que componen a los diferentes vestigios Patrimoniales, sobre todo en el Estado de Chihuahua, el cual se caracteriza por una amplia diversidad climática.
             </p>
             <br>
-            <p>El programa de investigación de CIE@Lab tiene como objetivo desarrollar protocolos para la determinación de indicadores físicoquímicos y biológicos para el diagnóstico de los inmuebles y con ello atender demandas de conservación específicas. Así mismo, el programa de investigación pretende generar estrategias, que empleen el conocimiento científico multidisciplinario, para la generación de técnicas y productos que ayuden a la conservación y restauración de los inmuebles de Patrimonio Histórico de acuerdo a la composición y tipo de deterioro. Además, la investigación material del patrimonio tangible aporta nuevos datos sobre la historia socioeconómica, y artística en la que fueron pensados y construidos. 
+            <p>
+El programa de investigación de CIE@Lab tiene como objetivo desarrollar protocolos para la determinación de indicadores físicoquímicos y biológicos para el diagnóstico de los inmuebles y con ello atender demandas de conservación específicas. Así mismo, el programa de investigación busca generar estrategias que empleen el conocimiento científico multidisciplinario, para la generación de técnicas y productos que ayuden a la conservación y restauración de los inmuebles de Patrimonio Histórico, de acuerdo a la composición y tipo de deterioro. Además, la investigación material del patrimonio tangible aporta nuevos datos sobre la historia socioeconómica, y artística en la que fueron pensados y construidos.
             </p>
             <br>
             <ul>
               <h4>Actividades principales:</h4>
-              <li>Establecer protocolos para la toma de muestra de diferentes materiales que componen los inmuebles para su análisis químico, físico y biológico.
+              <li>
+Establecer protocolos para la toma de muestra de diferentes materiales que componen los inmuebles para su análisis químico, físico y biológico.
               </li>
-              <li>Establecer protocolos para el análisis de las muestras a través de técnicas de caracterización de materiales que ayuden a diagnosticar el deterioro.
+              <li>
+Establecer protocolos para el análisis de las muestras a través de técnicas de caracterización de materiales que ayuden a diagnosticar el deterioro
               </li>
-              <li>Establecer protocolos para el análisis de las muestras a nivel microbiológico para diagnosticar el biodeterioro.
+              <li>
+Establecer protocolos para el análisis de las muestras a nivel microbiológico para diagnosticar el biodeterioro.
               </li>
-              <li>Generar indicadores químicos, físicos y biológicos para el diagnóstico de deterioro de los inmuebles de Patrimonio Histórico.
+              <li>
+Generar indicadores químicos, físicos y biológicos para el diagnóstico de deterioro de los inmuebles de Patrimonio Histórico.
               </li>
-              <li>Generar reporte técnico de los resultados.
+              <li>
+              Generar reporte técnico de los resultados.
               </li>
-              <li>Generar un Manual de protocolo para la investigación en materia de conservación y preservación de Patrimonio Histórico. 
+              <li>
+Generar un manual de protocolo para la investigación en materia de conservación y preservación de Patrimonio Histórico.
               </li>
             </ul>
           </div></li>`
